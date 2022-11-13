1d4155babc7b34807cfa694af4dd6acef6bdc32b
abanoub.samirfahmi@gmail.com <abanoub.samirfahmi@gmail.com>
2022-11-14 00:21:00 +0200

    apply the location reminder test 1) Create a fake data source to act as a double to the real data source 2) getReminders 3) saveReminder 4) getReminder by ID 5) deleteAllReminders
    implement RemindersListViewModelTest
    
    1) create new class MainCoroutine & live data function
    maincoroutine conatins two main functions (start / finish) by using dispatcher
    2) getReminders
    3) saveReminder
    4) getReminder by ID
    5) deleteAllReminders
    
    implement testing to the SaveReminderView and its live data objects
    
    1) check_loading
    fakeDataSource = FakeDataSource()
            saveReminderViewModel
            mainCoroutineRule.pauseDispatcher()
            validateAndSaveReminder(firstReminder)
            Assert.assertThat
    
    2) check error if title is null as a sample

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,26 +3,50 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
+
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
-
+    //    Done: Create a fake data source to act as a double to the real data source
+    var error_flag = false
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        //Done("Return the reminders")
+        if (error_flag) {
+            return Result.Error(
+                "Error is happening during get the reminders"
+            )
+        }
+        remindersList?.let { return Result.Success(it) }
+        return Result.Error("Reminders not exist")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        // Done("save the reminder")
+        remindersList?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        // Done("return the reminder with the id")
+        val reminder = remindersList?.find { reminderDTO ->
+            reminderDTO.id == id
+        }
+        return when {
+            error_flag -> {
+                Result.Error("Reminder not found!")
+            }
+
+            reminder != null -> {
+                Result.Success(reminder)
+            }
+            else -> {
+                Result.Error("Reminder not found!")
+            }
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        //Done("delete all the reminders")
+        remindersList?.clear()
     }
-
 
 }
